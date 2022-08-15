d3dd40b23a11684ab7b790e03183701c52ef180c
Dsribass <ds_ribas@hotmail.com>
2022-08-15 14:23:14 -0300

    feat: add view binding to project

--- app/src/main/java/com/example/cadastro/MainActivity.java
+++ app/src/main/java/com/example/cadastro/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.cadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewbinding.ViewBinding;
 
 import android.os.Bundle;
 
+import com.example.cadastro.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setupView();
+    }
+
+    private void setupView() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
